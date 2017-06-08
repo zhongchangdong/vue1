d1cd53a24037a8eea869ef3a69dc9bed531f3f7f
my <737>
2017-06-08 21:13:23 +0800

    first commit

---
+++ 01.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2017-06-05 20:53:51
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2017-06-05 21:11:15
+ */
+echo "我是数据";
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div >
+        {{msg}}
+    </div>
+
+</template>
+
+<script>
+       export default{
+        data(){
+             return{
+                msg:9
+             }
+        },
+
+    }
+
+
+
+</script>
+
+<style>
+
+    .blue{
+        color:yellow;
+    }
+    .red{
+     background:red;
+    }
+
+
+
+</style>
